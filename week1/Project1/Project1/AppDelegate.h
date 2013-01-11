931df8ce2cda21adf72aef75752aac02cd259021
Michelle Pessoa <mmpessoa@gmail.com>
2013-01-10 23:39:28 -0500

    Pushing Xcode files to GitHub.

---
+++ week1/Project1/Project1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project1
+//
+//  Created by Michelle Pessoa on 1/10/13.
+//  Copyright (c) 2013 Michelle Pessoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
