807798712910ea0245a65bebd8700b9103791184
Michelle Pessoa <mmpessoa@gmail.com>
2013-01-24 22:00:52 -0500

    Uploading commented ViewController file.

--- week3/project3/project3/ViewController.m
+++ week3/project3/project3/ViewController.m
@@ -16,6 +16,73 @@
 
 - (void)viewDidLoad
 {
+    
+    //Create a function called Add. This function will take two NSInteger or int types and return the result of an addition between these two.
+    
+    
+    
+    
+    
+    
+    //Create a BOOL function called Compare that takes two NSInteger values. Return YES or NO based on whether the values are equal.
+    
+    
+    
+    
+    
+    //Create a function called Append. This function will take two NSStrings and return a new NSString containing the appended strings using an NSMutableString and the Append method.
+    
+    
+    
+    
+    
+    
+    //Call the Append function with two NSStrings. Capture the result and display a UIAlertView with the appended string using displayAlertWithString.
+    
+    
+    
+    
+    
+    
+    //Create a function called DisplayAlertWithString. This function will take as a parameter an NSString.
+    
+    
+    
+    
+    
+    
+    //Call the Add function passing in two integer values. Capture the return of this function into a variable.
+    
+    
+    
+    
+    
+    
+    //Bundle the returned integer into an NSNumber and then convert it to a NSString and pass it to the DisplayAlertWithString function.
+    
+    
+    
+    
+    
+    
+    //Give it some text for the title. The message will read, "The number is 00". Replace the 00 with the integer passed into the function.
+    
+    
+    
+    
+    
+    
+    //Call the Compare function with two integer values. If Compare returns YES, display an UIAlertView both with the input values and the result using the DisplayAlertWithString function.
+    
+    //DisplayAlertWithString Function
+    //1. Take the passed in NSString and display it in the alert view
+    //2. Create an UIAlertView
+    
+    
+    
+    
+    
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
